2efc27f6142bafa480dd62d49aa040fb49da09e0
akash-k-s <akash2001ks@gmail.com>
2024-03-07 11:44:23 +0530

    rewrote threads code

--- Test.m
+++ Test.m
@@ -1,5 +1,6 @@
-currentFolder = pwd;
-fileIdx ='0000';
-folderName = 'D:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\PostProc\TEST_data_3\master_0000_idx.bin';
-
-
+current_directory = pwd;
+cd D:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\Scripts\Cascade\ ;
+fid = fopen('Cascade_Configuration_MIMO.lua');
+fseek(fid,20,'bof');
+fgetl(fid) 
+cd(current_directory);
--- synchronization.m
+++ synchronization.m
@@ -5,24 +5,16 @@ addpath(genpath('.\'))
 %% configuring radar parameters
 f1 = parfeval(@radar_capture,1,1)
 T = fetchOutputs(f1);
-
 %% radar capure and motor control
 count = 1
 max_steps = 4;
-FunList ={@radar_capture, @motorcontroller};
-outputlist={};
 serial_connection = true;
-while(serial_connection)
-    if(max_steps > count)
+while(max_steps > count)
         f1 = parfeval(@radar_capture,1,2)
         f2 = parfeval(@motorcontroller,1,count)
         T = fetchOutputs(f1);
         T = fetchOutputs(f2);
+        % add y axis movement 
         count = count +1
-        dataList{2}{2}= count;
-    else
-        serial_connection = false;
-    end
-
 end
 
