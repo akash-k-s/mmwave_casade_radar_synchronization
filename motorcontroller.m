1de1e3f3e6cb935e8ebe72b77a9b69acb9cf46e1
akash-k-s <akash2001ks@gmail.com>
2024-03-22 11:18:44 +0530

    updated Gcode to G01 for goals

--- motorcontroller.m
+++ motorcontroller.m
@@ -5,13 +5,13 @@ configureTerminator(controller,"CR");
 %%  finding the direction of the motor
 direction = count
 %% pause for 2 seconds for radar to intilize
-pause(2);
+pause(10);
 if mod(direction, 2)
     disp('moving in -x direction')
-    writeline(controller,"G01X-200F10")
+    writeline(controller,"G01X-1000F1000")
 else
     disp('moving in +x direction')
-    writeline(controller,"G01X200F10")
+    writeline(controller,"G01X0F1000")
 end
 %% reading  the output from serial 
 read(controller,1000,"string");
--- synchronization.m
+++ synchronization.m
@@ -6,15 +6,11 @@ addpath(genpath('.\'))
 f1 = parfeval(@radar_capture,1,1)
 T = fetchOutputs(f1);
 %% radar capure and motor control
-count = 1
-max_steps = 4;
-serial_connection = true;
-while(max_steps > count)
-        f1 = parfeval(@radar_capture,1,2)
-        f2 = parfeval(@motorcontroller,1,count)
-        T = fetchOutputs(f1);
-        T = fetchOutputs(f2);
-        % add y axis movement 
-        count = count +1
-end
+count = 2
+f1 = parfeval(@radar_capture,1,2)
+f2 = parfeval(@motorcontroller,1,count)
+T = fetchOutputs(f1);
+T = fetchOutputs(f2);
+% add y axis movement 
+count = count +1
 
